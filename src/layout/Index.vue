0a486cd1844f85728a0fa3803500e2b19f94b98c
xianyu <3186025662@qq.com>
2024-10-11 13:42:06 +0800

    完成个人中心

--- src/layout/Index.vue
+++ src/layout/Index.vue
@@ -2,103 +2,78 @@
   <Layout class=" !h-[100vh]">
     <LayoutSider width="50" class="!bg-[#f8f9fc]" style="border-right: 1px solid #c9ceda;">
       <div class="text-center">
-          <Icon
-            icon="icon-park-outline:workbench"
-            style="color: black"
-            class="text-[2vw] ml-[0.5vw] mt-[2vw]"
-          />工作台
-        </div>
+        <Icon icon="icon-park-outline:workbench"
+        style="color: black" class="text-[2vw] ml-[0.5vw] mt-[2vw]" />工作台
+      </div>
+      <div class="text-center">
+        <Icon icon="tdesign:folder-1"
+        style="color: black" class="text-[2vw] ml-[0.5vw] mt-[1vw]" />项目
+      </div>
+      <div class="text-center">
+        <Icon icon="ph:open-ai-logo-fill"
+        style="color: black" class="text-[2vw] ml-[0.5vw] mt-[1vw]" />AI
+      </div>
+      <div class="text-center">
+        <Icon icon="ph:file-text-duotone"
+        style="color: black" class="text-[2vw] ml-[0.5vw] mt-[1vw]" />事项
+      </div>
+      <RouterLink to="/based">
         <div class="text-center">
-          <Icon
-            icon="tdesign:folder-1"
-            style="color: black"
-            class="text-[2vw] ml-[0.5vw] mt-[1vw]"
-          />项目
+          <Icon icon="ph:code-fill" style="color: black" class="text-[2vw] ml-[0.5vw] mt-[1vw]" />代码
         </div>
-        <div class="text-center">
-          <Icon
-            icon="ph:open-ai-logo-fill"
-            style="color: black"
-            class="text-[2vw] ml-[0.5vw] mt-[1vw]"
-          />AI
-        </div>
-        <div class="text-center">
-          <Icon
-            icon="ph:file-text-duotone"
-            style="color: black"
-            class="text-[2vw] ml-[0.5vw] mt-[1vw]"
-          />事项
-        </div>
-        <RouterLink to="/based">
-          <div class="text-center">
-          <Icon
-            icon="ph:code-fill"
-            style="color: black"
-            class="text-[2vw] ml-[0.5vw] mt-[1vw]"
-          />代码
-        </div>
-        </RouterLink>
+      </RouterLink>
 
-        <div class="text-center">
-          <Icon
-            icon="gravity-ui:cube"
-            style="color: black"
-            class="text-[2vw] ml-[0.5vw] mt-[1vw]"
-          />
-          制品
-        </div>
-        <div class="text-center">
-          <Icon
-            icon="ic:baseline-insights"
-            style="color: black"
-            class="text-[2vw] ml-[0.5vw] mt-[1vw]"
-          />
-          洞察
-        </div>
-        <div class="text-center">
-          <Icon
-            icon="akar-icons:book"
-            style="color: black"
-            class="text-[2vw] ml-[0.5vw] mt-[1vw]"
-          />
-          知识
-        </div>
-        <div class="text-center">
-          <Icon
-            icon="mdi:automatic"
-            style="color: black"
-            class="text-[2vw] ml-[0.5vw] mt-[1vw]"
-          />
-          自动化
-        </div>
+      <div class="text-center">
+        <Icon icon="gravity-ui:cube" style="color: black" class="text-[2vw] ml-[0.5vw] mt-[1vw]" />
+        制品
+      </div>
+      <div class="text-center">
+        <Icon icon="ic:baseline-insights"
+        style="color: black" class="text-[2vw] ml-[0.5vw] mt-[1vw]" />
+        洞察
+      </div>
+      <div class="text-center">
+        <Icon icon="akar-icons:book" style="color: black" class="text-[2vw] ml-[0.5vw] mt-[1vw]" />
+        知识
+      </div>
+      <div class="text-center">
+        <Icon icon="mdi:automatic" style="color: black" class="text-[2vw] ml-[0.5vw] mt-[1vw]" />
+        自动化
+      </div>
     </LayoutSider>
     <Layout>
       <LayoutHeader class=" !h-[10vh] !bg-white" style="border-bottom: 1px solid #c9ceda;">
         <div class="text-[1.5vw] font-semibold leading-[4.5vw]">
-            代码仓库
+          代码仓库
         </div>
       </LayoutHeader>
       <Layout>
         <LayoutContent class="!p-[10px] !box-border">
           <div>
-            <router-link
-              to="/basea"
+            <router-link to="/basea"
               class="w-[13vw]
-              h-[3vw] inline-block text-[1.3vw] focus:border-b-2 border-[#306fde] text-center"
-              >最近访问</router-link
-            ><router-link
-              to="/baseb"
+              h-[3vw] inline-block text-[1.3vw]
+              focus:border-b-2 border-[#306fde]
+              text-center">最近访问</router-link><router-link to="/baseb"
               class="w-[13vw] h-[3vw]
-              inline-block text-[1.3vw] focus:border-b-2 border-[#306fde] text-center"
-              >全部仓库</router-link
-            ><router-link
-              to="/basec"
-              class="w-[13vw] h-[3vw]
-              inline-block text-[1.3vw] focus:border-b-2 border-[#306fde] text-center"
-              >开源仓库</router-link
-            >
+              inline-block text-[1.3vw] f
+              ocus:border-b-2 border-[#306fde] text-center">全部仓库</router-link><router-link
+              to="/basec" class="w-[13vw] h-[3vw]
+              inline-block
+              text-[1.3vw] focus:border-b-2 border-[#306fde] text-center">开源仓库</router-link>
+            <Button type="primary" @click="showDrawer">个人中心</Button>
+            <Drawer v-model:open="open" class="custom-class" root-class-name="root-class-name"
+              :root-style="{ color: 'blue' }"
+              style="color: red" title="Basic Drawer" placement="left"
+              @after-open-change="afterOpenChange">
+              <p>Some contents...</p>
+              <p>Some contents...</p>
+              <p>Some contents...</p>
+            </Drawer>
           </div>
-          <div> <RouterView></RouterView></div>
+          <div>
+            <RouterView></RouterView>
+          </div>
         </LayoutContent>
       </Layout>
     </Layout>
@@ -107,6 +82,16 @@
 <script lang="ts" setup>
 import {
   Layout, LayoutContent, LayoutHeader, LayoutSider,
+  Button, Drawer,
 } from "ant-design-vue";
+import { ref } from "vue";
 import { Icon } from "@iconify/vue";
+
+const open = ref(false);
+const afterOpenChange = (bool) => {
+  console.log("open", bool);
+};
+const showDrawer = () => {
+  open.value = true;
+};
 </script>
